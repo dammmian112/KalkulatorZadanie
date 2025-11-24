4c2c74235c5b005ace34ab2e5b5d3f0e6c102c77
Krzysziu Juszczak <kr.juszczak@student.po.edu.pl>
2025-11-24 13:48:09 +0100

    dodano funkcjonalnoc mnozenie

---
+++ mnozenie.py
@@ -0,0 +1,17 @@
+def mnozenie(a, b):
+    """
+    Funkcja mnożąca dwie liczby.
+    
+    Args:
+        a (float): Pierwsza liczba
+        b (float): Druga liczba
+    
+    Returns:
+        float: Wynik mnożenia (a * b)
+    """
+    return a * b
+
+def main():
+    
+        wynik_mnozenia = mnozenie(liczba1, liczba2)
+        print(f"Wynik mnożenia: {liczba1} * {liczba2} = {wynik_mnozenia}")
